31833a23156b4f526aa87316da4985e988de97d0
nkosi-tauro <nkosilati23@yahoo.com>
2021-02-18 19:04:06 +0200

    added keys to .env

--- src/main.js
+++ src/main.js
@@ -5,15 +5,14 @@ import './assets/tailwind.css'
 import firebase from "firebase"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC5bJaMF6XAmr3Z4XlfAHeHAOIdI_iJzCw",
-    authDomain: "test-63eb7.firebaseapp.com",
+    apiKey: process.env.VUE_APP_APIKEY,
+    authDomain: process.env.VUE_APP_AUTH_DOMAIN,
     databaseURL: "https://test-63eb7.firebaseio.com",
-    projectId: "test-63eb7",
+    projectId: process.env.VUE_APP_PROJECT_ID,
     storageBucket: "test-63eb7.appspot.com",
-    messagingSenderId: "601897179957",
-    appId: "1:601897179957:web:8e35d0edeafc71ae5bca7c"
+    messagingSenderId: process.env.VUE_APP_SENDER_ID,
+    appId: process.env.VUE_APP_APP_ID
 };
-
 firebase.initializeApp(firebaseConfig)
 
 createApp(App).use(router).mount('#app')
