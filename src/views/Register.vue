a79ba7f66e9c13caacc2ee10cab7d72c9e17bbc0
nkosi-tauro <nkosilati23@yahoo.com>
2021-02-18 16:59:02 +0200

    update register page

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -27,7 +27,7 @@
                 type="submit"
                 class="transition duration-200 bg-blue-500 hover:bg-blue-600 focus:bg-blue-700 focus:shadow-sm focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50 text-white w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-semibold text-center inline-block"
               >
-                <span class="inline-block mr-2">Login</span>
+                <span class="inline-block mr-2">Register</span>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
