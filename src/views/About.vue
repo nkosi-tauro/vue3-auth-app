3db3be61f899114806414e6747ea8cddd6bb1674
nkosi-tauro <nkosilati23@yahoo.com>
2021-02-18 18:14:26 +0200

    add header component

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="about">
+    <Header/>
     <h1>This is an about page</h1>
   </div>
 </template>
+
+
+<script>
+import Header from "../components/Header.vue";
+export default {
+  name : "About",
+  components: {
+    Header
+  }
+  
+}
+</script>
