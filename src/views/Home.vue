0a62e313f81b600eb06f83938a1b575cd0137fec
nkosi-tauro <nkosilati23@yahoo.com>
2021-02-18 18:08:25 +0200

    added firebaseLogout & Header

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,72 @@
 <template>
   <div class="home">
-    <h1>This is an home page</h1>
+    <div class="min-h-screen">
+      <div class="antialiased bg-gray-100 dark-mode:bg-gray-900">
+        <div
+          class="w-full text-gray-700 bg-white dark-mode:text-gray-200 dark-mode:bg-gray-800"
+        >
+          <div
+            class="flex flex-col max-w-screen-xl px-4 mx-auto md:items-center md:justify-between md:flex-row md:px-6 lg:px-8"
+          >
+            <div class="flex flex-row items-center justify-between p-4">
+              <a
+                href="#"
+                class="text-lg font-semibold tracking-widest text-gray-900 uppercase rounded-lg dark-mode:text-white focus:outline-none focus:shadow-outline"
+                >Authentication App</a
+              >
+            </div>
+            {{name}}
+            <nav
+              
+              class="flex-col flex-grow hidden pb-4 md:pb-0 md:flex md:justify-end md:flex-row"
+            >
+   
+              <router-link to="/about"
+                class="px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
+          
+                >Profile {{name}}</router-link
+              >
+              <a
+                class="px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
+                @click="Logout"
+                >Logout</a
+              >
+            </nav>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+import {onBeforeMount, reactive, toRefs} from 'vue'
+import firebase from 'firebase'
+
+export default {
+  name: 'Home',
+  setup(props) {
+
+    const state = reactive({
+      name : ""
+    })
+
+    onBeforeMount(() => {
+      const user = firebase.auth().currentUser;
+      if(user){
+        state.name = user.email.split("@")[0]
+      }
+    })
+
+    function Logout(){
+      firebase
+        .auth()
+        .signOut()
+    }
+
+    return {...toRefs(state), Logout}
+  }
+}
+</script>
+
+
