06454093b2c0c11c4e8f7577719475bc553213f7
nkosi-tauro <nkosilati23@yahoo.com>
2021-02-18 18:43:37 +0200

    added dynamic router-links

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="header">
-    <div class="min-h-screen">
+    <div class="">
       <div class="antialiased bg-gray-100 dark-mode:bg-gray-900">
         <div
           class="w-full text-gray-700 bg-white dark-mode:text-gray-200 dark-mode:bg-gray-800"
@@ -15,17 +15,21 @@
                 >Authentication App</a
               >
             </div>
-            {{name}}
             <nav
-              
               class="flex-col flex-grow hidden pb-4 md:pb-0 md:flex md:justify-end md:flex-row"
             >
-   
-              <router-link to="/about"
+              <router-link
+                v-if="!currentRoute"
+                to="/about"
                 class="px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
-          
-                >Profile {{name}}</router-link
-              >
+                >Profile {{ name }}
+              </router-link>
+              <router-link
+                v-else-if="currentRoute"
+                to="/"
+                class="px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
+                >Home
+              </router-link>
               <a
                 class="px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 md:ml-4 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
                 @click="Logout"
@@ -40,33 +44,37 @@
 </template>
 
 <script>
-import {onBeforeMount, reactive, toRefs} from 'vue'
-import firebase from 'firebase'
-
+import { onBeforeMount, reactive, toRefs } from "vue";
+import firebase from "firebase";
+import { useRoute, useRouter } from "vue-router";
 export default {
-  name: 'Header',
+  name: "Header",
   setup(props) {
-
+    const route = useRoute();
+    const router = useRouter();
     const state = reactive({
-      name : ""
-    })
+      name: "",
+      currentRoute: true,
+    });
 
     onBeforeMount(() => {
       const user = firebase.auth().currentUser;
-      if(user){
-        state.name = user.email.split("@")[0]
+      if (user) {
+        state.name = user.email.split("@")[0];
       }
-    })
-
-    function Logout(){
-      firebase
-        .auth()
-        .signOut()
+    });
+    // Dynamic route
+    if (route.path !== "/about") {
+      state.currentRoute = false;
     }
 
-    return {...toRefs(state), Logout}
-  }
-}
+    function Logout() {
+      firebase.auth().signOut();
+    }
+
+    return { ...toRefs(state), Logout };
+  },
+};
 </script>
 
 
