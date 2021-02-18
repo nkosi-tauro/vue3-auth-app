b21a7c3de2ce6fa45a0ffb1206240da8b1fac026
nkosi-tauro <nkosilati23@yahoo.com>
2021-02-19 01:41:58 +0200

    added profile forms

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
-    <Header />
+    <Header/>
     <!-- component -->
     <section class="py-10 bg-gray-100 bg-opacity-50 h-screen">
       <div class="mx-auto container max-w-2xl md:w-3/4 shadow-md">
@@ -15,7 +15,7 @@
                 src="https://avatars3.githubusercontent.com/u/72724639?s=400&u=964a4803693899ad66a9229db55953a3dbaad5c6&v=4"
               />
 
-              <h1 class="text-gray-600">Charly Olivas</h1>
+              <h1 class="text-gray-600">Charly Olivas {{name}}</h1>
             </div>
           </div>
         </div>
@@ -118,10 +118,10 @@
             <h2 class="md:w-4/12 max-w-sm mx-auto">Change password</h2>
 
             <div
-              class="md:w-5/12 w-full md:pl-9 max-w-sm mx-auto space-y-5 md:inline-flex pl-2"
+              class=" w-full  max-w-sm mx-auto space-y-5 md:inline-flex pl-2"
             >
-              <div class="w-full inline-flex border-b">
-                <div class="w-1/12 pt-2">
+          <div class="w-full inline-flex border-b">
+                <div class="w-1/12 pt-2 bg-gray-100">
                   <svg
                     fill="none"
                     class="w-6 text-gray-400 mx-auto"
@@ -138,52 +138,20 @@
                 </div>
                 <input
                   type="password"
-                  class="w-11/12 focus:outline-none focus:text-gray-600 p-2 ml-4"
+                  class="w-11/12 focus:outline-none focus:text-gray-600 p-2 "
                   placeholder="New"
+                  disabled
                 />
               </div>
-            </div>
-
-            <div class="md:w-3/12 text-center md:pl-6">
-              <button
-                class="text-white w-full mx-auto max-w-sm rounded-md text-center bg-indigo-400 py-2 px-4 inline-flex items-center focus:outline-none md:float-right"
-              >
-                <svg
-                  fill="none"
-                  class="w-4 text-white mr-2"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
-                  />
-                </svg>
-                Update
-              </button>
             </div>
           </div>
 
           <hr />
           <div class="w-full p-4 text-right text-gray-500">
-            <button class="inline-flex items-center focus:outline-none mr-4">
-              <svg
-                fill="none"
-                class="w-4 mr-2"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-                />
-              </svg>
-              Delete account
+            <button class="inline-flex rounded-md text-center text-white py-2 px-4 bg-indigo-400 items-center focus:outline-none mr-4">
+              Save
             </button>
+       
           </div>
         </div>
       </div>
@@ -199,5 +167,8 @@ export default {
   components: {
     Header,
   },
+  props:{
+    name : String
+  }
 };
 </script>
