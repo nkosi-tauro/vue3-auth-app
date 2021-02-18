b5a821cad70376601c1f3bde0656a63907897fd6
nkosi-tauro <nkosilati23@yahoo.com>
2021-02-18 16:37:19 +0200

    add new login form

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,109 +1,82 @@
 <template>
-  <div class="login absolute w-full top-0 bg-gray-900">
-    <div
-      class="absolute top-0 w-full h-full "
-    ></div>
-    <div class="container mx-auto px-4 h-full">
-      <div class="flex content-center items-center justify-center h-full">
-        <div class="w-full lg:w-4/12 px-4 pt-32">
-          <div
-            class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-gray-300 border-0"
-          >
-            <div class="rounded-t mb-0 px-6 py-6">
-              <div class="text-center mb-3">
-                <h6 class="text-gray-600 text-sm font-bold">Sign in with</h6>
-              </div>
-              <div class="btn-wrapper text-center">
-                <button
-                  class="bg-white active:bg-gray-100 text-gray-800 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-2 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs"
-                  type="button"
-                  style="transition: all 0.15s ease 0s"
+  <div class="login">
+    <!-- component -->
+    <div class="min-h-screen bg-gray-100 flex flex-col justify-center sm:py-12">
+      <div class="p-10 xs:p-0 mx-auto md:w-full md:max-w-md">
+        <h1 class="font-bold text-center text-2xl mb-5">Authentication App</h1>
+        <div class="bg-white shadow w-full rounded-lg divide-y divide-gray-200">
+          <div class="px-5 py-7">
+            <form @submit.prevent="Login">
+              <label class="font-semibold text-sm text-gray-600 pb-1 block"
+                >E-mail</label
+              >
+              <input
+                v-model.trim="email"
+                type="text"
+                class="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
+              />
+              <label class="font-semibold text-sm text-gray-600 pb-1 block"
+                >Password</label
+              >
+              <input
+                v-model.trim="password"
+                type="text"
+                class="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
+              />
+              <button
+                type="submit"
+                class="transition duration-200 bg-blue-500 hover:bg-blue-600 focus:bg-blue-700 focus:shadow-sm focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50 text-white w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-semibold text-center inline-block"
+              >
+                <span class="inline-block mr-2">Login</span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  class="w-4 h-4 inline-block"
                 >
-                  <img
-                    alt="..."
-                    class="w-5 mr-1"
-                    src="../assets/github.svg"
-                  />Github
-                </button>
-                <button
-                  class="bg-white active:bg-gray-100 text-gray-800 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-1 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs"
-                  type="button"
-                  style="transition: all 0.15s ease 0s"
-                >
-                  <img
-                    alt="..."
-                    class="w-5 mr-1"
-                    src="../assets/google.svg"
-                  />Google
-                </button>
-              </div>
-              <hr class="mt-6 border-b-1 border-gray-400" />
-            </div>
-            <div class="flex-auto px-4 lg:px-10 py-10 pt-0">
-              <div class="text-gray-500 text-center mb-3 font-bold">
-                <small>Or sign in with credentials</small>
-              </div>
-              <form>
-                <div class="relative w-full mb-3">
-                  <label
-                    class="block uppercase text-gray-700 text-xs font-bold mb-2"
-                    for="grid-password"
-                    >Email</label
-                  >
-                  <input
-                    type="email"
-                    class="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full"
-                    placeholder="Email"
-                    style="transition: all 0.15s ease 0s"
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M17 8l4 4m0 0l-4 4m4-4H3"
                   />
-                </div>
-                <div class="relative w-full mb-3">
-                  <label
-                    class="block uppercase text-gray-700 text-xs font-bold mb-2"
-                    for="grid-password"
-                    >Password</label
-                  >
-                  <input
-                    type="password"
-                    class="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full"
-                    placeholder="Password"
-                    style="transition: all 0.15s ease 0s"
-                  />
-                </div>
-                <div>
-                  <label class="inline-flex items-center cursor-pointer">
-                    <input
-                      id="customCheckLogin"
-                      type="checkbox"
-                      class="form-checkbox text-gray-800 ml-1 w-5 h-5"
-                      style="transition: all 0.15s ease 0s"
-                    /><span class="ml-2 text-sm font-semibold text-gray-700"
-                      >Remember me</span
-                    ></label
-                  >
-                </div>
-                <div class="text-center mt-6">
-                  <button
-                    class="bg-gray-900 text-white active:bg-gray-700 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full"
-                    type="button"
-                    style="transition: all 0.15s ease 0s"
-                  >
-                    Sign In
-                  </button>
-                </div>
-              </form>
+                </svg>
+              </button>
+            </form>
+          </div>
+          <div class="p-5">
+            <div class="grid grid-cols-2 gap-1">
+              <button
+                type="button"
+                class="transition duration-200 border border-gray-200 text-gray-500 w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-normal text-center inline-block"
+              >
+                Google
+              </button>
+              <button
+                type="button"
+                class="transition duration-200 border border-gray-200 text-gray-500 w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-normal text-center inline-block"
+              >
+                Github
+              </button>
             </div>
           </div>
-          <div class="flex flex-wrap mt-6">
-            <div class="w-1/2">
-              <a href="#pablo" class="text-gray-300"
-                ><small>Forgot password?</small></a
-              >
-            </div>
-            <div class="w-1/2 text-right">
-              <a href="#pablo" class="text-gray-300"
-                ><small>Create new account</small></a
-              >
+          <div class="py-5">
+            <div class="grid grid-cols-2 gap-1">
+              <div class="text-center sm:text-left whitespace-nowrap">
+                <button
+                  class="transition duration-200 mx-5 px-5 py-4 cursor-pointer font-normal text-sm rounded-lg text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-200 focus:ring-2 focus:ring-gray-400 focus:ring-opacity-50 ring-inset"
+                >
+                  <span class="inline-block ml-1">Forgot Password</span>
+                </button>
+              </div>
+              <div class="text-center sm:text-right whitespace-nowrap">
+                <router-link to="/register">
+                    <button class="transition duration-200 mx-5 px-5 py-4 cursor-pointer font-normal text-sm rounded-lg text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-200 focus:ring-2 focus:ring-gray-400 focus:ring-opacity-50 ring-inset">
+                        <span class="inline-block ml-1">Create Account</span>
+                    </button>
+                </router-link>
+              </div>
             </div>
           </div>
         </div>
@@ -111,6 +84,7 @@
     </div>
   </div>
 </template>
+
 
 <script>
 export default {
